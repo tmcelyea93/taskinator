daa78163dab4f69fd10e56e40367eff70b33fbfb
tmcelyea93 <tarahmcelyea@gmail.com>
2020-11-25 17:01:41 -0600

    styling changes to drag and drop

--- assets/js/script.js
+++ assets/js/script.js
@@ -190,6 +190,7 @@ var taskButtonHandler = function(event) {
     var taskListEl = event.target.closest(".task-list");
     if (taskListEl) {
       event.preventDefault();
+      taskListEl.setAttribute("style", "background: rgba(68, 233, 255, 0.7); border-style: dashed;");
     }
   };
 
@@ -210,8 +211,15 @@ var taskButtonHandler = function(event) {
     else if (statusType === "tasks-completed") {
       statusSelectEl.selectedIndex = 2;
     }
-
+    dropZoneEl.removeAttribute("style");
     dropZoneEl.appendChild(draggableElement);
+  };
+
+  var dragLeaveHandler = function(event) {
+    var taskListEl = event.target.closest(".task-list");
+    if (taskListEl) {
+    taskListEl.removeAttribute("style");
+   }
   };
 
 pageContentEl.addEventListener("click", taskButtonHandler);
@@ -219,3 +227,4 @@ pageContentEl.addEventListener("change", taskStatusChangeHandler);
 pageContentEl.addEventListener("dragstart", dragTaskHandler);
 pageContentEl.addEventListener("dragover", dropZoneDragHandler);
 pageContentEl.addEventListener("drop", dropTaskHandler);
+pageContentEl.addEventListener("dragleave", dragLeaveHandler);
